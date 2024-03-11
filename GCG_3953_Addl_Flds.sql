5f31a047281d1c86274b6562df124e671fd90de3
Service Web <sw@gssmail.com>
2024-03-11 10:01:44 -0500

    Commit changes in C:\Websites\Git\emarlow\3953\GCG_3953_Addl_Flds.sql

---
+++ GCG_3953_Addl_Flds.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_3953_Addl_Flds"(
+ "SO" CHAR(7),
+ "Line" CHAR(4),
+ "Dept" INTEGER,
+ "Approved" BIT,
+ "User_ID" INTEGER,
+ "GS_User" CHAR(8),
+ "Date_Approval" DATE,
+ "Comments" LONGVARCHAR);
